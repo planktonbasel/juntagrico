83f31d469fa008a9c663ee97692ae6f1bc785ec7
David Glenck <github@ihdg.ch>
2026-05-17 22:27:04 +0200

    Fix end date to include the entire month of December

--- planktonbasel/templatetags/utils.py
+++ planktonbasel/templatetags/utils.py
@@ -14,5 +14,5 @@ def assignment_progress(member, future=None, start=None, end=None, subscription=
             end = datetime.date(2026, 4, 30)
         elif today < datetime.date(2027, 1, 1):
             start = datetime.date(2026, 5, 1)
-            end = datetime.date(2026, 12, 1)
+            end = datetime.date(2026, 12, 31)
     return widgets.assignment_progress(member, future, start, end, subscription)
